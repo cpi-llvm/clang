4222dd61fed7873efc2474b7745d8188f6bbfb27
Alexandre Bique <bique.alexandre@gmail.com>
2014-11-10 16:59:36 +0100

    [safestack] update driver test and codegen test

--- test/Driver/safestack.c
+++ test/Driver/safestack.c
@@ -2,21 +2,5 @@
 // NOSP-NOT: "-stack-protector" "4"
 
 // RUN: %clang -fsafe-stack -### %s 2>&1 | FileCheck %s -check-prefix=SP
+// RUN: %clang -fsanatizer=address -fsafe-stack -### %s 2>&1 | FileCheck %s -check-prefix=SP
 // SP: "-stack-protector" "4"
-
-// RUN: %clang     -target i386-unknown-unknown -fsafe-stack %s -S -emit-llvm -o - | FileCheck %s
-// RUN: %clang -O1 -target i386-unknown-unknown -fsafe-stack %s -S -emit-llvm -o - | FileCheck %s
-// RUN: %clang -O2 -target i386-unknown-unknown -fsafe-stack %s -S -emit-llvm -o - | FileCheck %s
-// RUN: %clang -O3 -target i386-unknown-unknown -fsafe-stack %s -S -emit-llvm -o - | FileCheck %s
-// RUN: %clang     -target i386-unknown-unknown -fno-safe-stack %s -S -emit-llvm -o - | FileCheck %s -check-prefix=NOSP2
-// RUN: %clang -O1 -target i386-unknown-unknown -fno-safe-stack %s -S -emit-llvm -o - | FileCheck %s -check-prefix=NOSP2
-
-// RUN: %clang -O2 -target i386-unknown-unknown -fno-safe-stack %s -S -emit-llvm -o - | FileCheck %s -check-prefix=NOSP2
-
-// RUN: %clang -O3 -target i386-unknown-unknown -fno-safe-stack %s -S -emit-llvm -o - | FileCheck %s -check-prefix=NOSP2
-
-// Verify that -fno-safe-stack invokes safestack instrumentation.
-
-int foo(int *a) { return *a; }
-// CHECK: attributes{{.*}}safestack
-// NOSP2-NOT: attributes{{.*}}safestack
