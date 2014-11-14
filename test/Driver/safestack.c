0a6a2aa1dce9f1291400f170ba73d4b3bb32d39a
Volodymyr Kuznetsov <vova.kuznetsov@epfl.ch>
2014-11-14 19:08:07 +0100

    Added two more test checks

--- test/Driver/safestack.c
+++ test/Driver/safestack.c
@@ -3,4 +3,6 @@
 
 // RUN: %clang -fsafe-stack -### %s 2>&1 | FileCheck %s -check-prefix=SP
 // RUN: %clang -fsanatizer=address -fsafe-stack -### %s 2>&1 | FileCheck %s -check-prefix=SP
+// RUN: %clang -fstack-protector -fsafe-stack -### %s 2>&1 | FileCheck %s -check-prefix=SP
+// RUN: %clang -fsafe-stack -fstack-protector-all -### %s 2>&1 | FileCheck %s -check-prefix=SP
 // SP: "-stack-protector" "4"
