590b6be0ef92083be42f53cb221a9a2ebc1ced68
Volodymyr Kuznetsov <ks.vladimir@gmail.com>
2015-04-15 23:30:14 +0200

    Renamed safestack builtins and make them library functions

--- lib/CodeGen/CGBuiltin.cpp
+++ lib/CodeGen/CGBuiltin.cpp
@@ -1632,14 +1632,6 @@ RValue CodeGenFunction::EmitBuiltinExpr(const FunctionDecl *FD,
         Builder.CreateAlignedLoad(IntToPtr, /*Align=*/4, /*isVolatile=*/true);
     return RValue::get(Load);
   }
-  case Builtin::BI__builtin_safestack_set_usp: {
-    Value *I = CGM.getIntrinsic(Intrinsic::safestack_set_usp);
-    return RValue::get(Builder.CreateCall(I, EmitScalarExpr(E->getArg(0))));
-  }
-  case Builtin::BI__builtin_safestack_get_usp: {
-    Value *I = CGM.getIntrinsic(Intrinsic::safestack_get_usp);
-    return RValue::get(Builder.CreateCall(I));
-  }
   }
 
   // If this is an alias for a lib function (e.g. __builtin_sin), emit
